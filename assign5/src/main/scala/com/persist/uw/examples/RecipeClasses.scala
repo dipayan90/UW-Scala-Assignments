fdc08e418f9453f941fac44b6e8c644796d0bc7c
Chattopadyay, Dipayan (DS) <Dipayan.Chattopadyay@cdk.com>
2016-11-19 12:11:38 -0800

    Adding intitial design elements

--- assign5/src/main/scala/com/persist/uw/examples/RecipeClasses.scala
+++ assign5/src/main/scala/com/persist/uw/examples/RecipeClasses.scala
@@ -14,6 +14,7 @@ object RecipeClasses {
     override def toString: String = f"$D${cents / 100}.${cents % 100}%02d"
   }
 
+
   trait Ingredient {
     val name: String
     val unit: String
@@ -26,5 +27,26 @@ object RecipeClasses {
   case class Cold(name: String, unit: String, pack: Int, price: Cost) extends Ingredient
 
   // Add the additional classes needed here
+
+  case class Action(name: String,ingredientList: List[Ingredient]) {
+    val home = new House
+
+  }
+
+   class House(){
+
+    val ingredientsAtHome = List.empty[Ingredient]
+
+    def getIngredients: List[Ingredient] =  ingredientsAtHome
+
+    def buy(ingredient: Ingredient) = {
+       ingredient :: ingredientsAtHome
+    }
+
+     def checkAvailability(ingredient: Ingredient) : Int = {
+       ingredientsAtHome.filter(e => e.equals(ingredient)).map(i => i.pack).sum
+     }
+
+  }
 
 }
